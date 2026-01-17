5aaa69ec6d4ab014050a4807e058f849b8e81438
apboats <adam@boatsbygeorge.com>
2026-01-17 00:59:40 -0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1914,7 +1914,6 @@ function InventoryBoatCard({ boat, onView }) {
     </div>
   );
 }
-}
 
 function BoatModal({ boat, locations, onSave, onCancel }) {
   // ====================================================================
